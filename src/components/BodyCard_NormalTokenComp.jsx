dbe09a1a0685e77091da1e4cb1efe3a3dc26cf83
FullTimeScam <418.gingin@gmail.com>
2024-05-27 23:34:55 +0900

    컴포넌트화 했다. 개고수플레이다. 캬

---
+++ src/components/BodyCard_NormalTokenComp.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+const BodyCard_NormalTokenComp = ({
+  imgNameWithExtention,
+  title,
+  ticker,
+  tokenAddress,
+  tokenAmount,
+}) => {
+  const [imgUrl, setImgUrl] = useState(`/images/${imgNameWithExtention}`);
+  const [isHover, setIsHover] = useState(false);
+
+  const shortenAddress = (tokenAddress) => {
+    return `${tokenAddress.slice(0, 4)}...${tokenAddress.slice(-4)}`;
+  };
+
+  const shortAddress = shortenAddress(tokenAddress);
+  const solscanUri = `https://solscan.io/token/${tokenAddress}`;
+
+  return (
+    <div className="flex justify-center items-start flex-col">
+      <div
+        role="button"
+        className="relative"
+        style={{ opacity: "1", transform: "none" }}
+      >
+        <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+          <div className="w-24 md:w-36 h-24 md:h-36">
+            <img
+              className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+              src={imgUrl}
+              alt={title}
+            />
+          </div>
+        </div>
+      </div>
+      {/* 여기부터가 텍스트영역 */}
+      <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+        <span
+          title={title}
+          className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+        >
+          ${title}
+        </span>
+        <a href={solscanUri} target="_blank">
+          <div className="flex flex-row items-center gap-x-2">
+            <img
+              title="Open in Solscan"
+              alt="Open in Solscan"
+              src="/images/Solscan.png"
+              className="h-3 md:h-5 rounded-full"
+            />
+            <span
+              title={tokenAddress}
+              className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+            >
+              {shortAddress}
+            </span>
+          </div>
+          <span
+            title="token amount"
+            className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+          >
+            {tokenAmount} {ticker}
+          </span>
+          <div>
+            <span
+              title="price"
+              className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+            >
+              {/* <span className="text-[#94E448]">&lt;$ </span>0.01 */}
+              <span className="text-[#94E448]">$ </span>
+              {tokenAmount}
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default BodyCard_NormalTokenComp;
---
+++ src/components/bin/BodyCard_NormalToken 컴포넌트화 시키기 전.jsx
@@ -0,0 +1,314 @@
+import { useState } from "react";
+
+const BodyCard_NormalToken = () => {
+  const [isHover, setIsHover] = useState(false);
+
+  return (
+    <div
+      className="flex items-center justify-center sm:justify-start flex-wrap gap-y-5 sm:gap-y-4 w-full mt-4 sm:mt-8"
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <div className="flex justify-center items-start flex-col">
+        <div
+          role="button"
+          className="relative"
+          style={{ opacity: "1", transform: "none" }}
+        >
+          <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+            <div className="w-24 md:w-36 h-24 md:h-36">
+              <img
+                className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+                src="/images/MEW.webp"
+                alt="$ZEUS airdrop | zeusonsol.org"
+              />
+            </div>
+          </div>
+        </div>
+        {/* 여기부터가 텍스트영역 */}
+        <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+          <span
+            title="cat in a dogs world"
+            className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+          >
+            cat in a dogs world
+          </span>
+          <a
+            href="https://solscan.io/token/MEW1gQWJ3nEXg2qgERiKu7FAFj79PHvQVREQUzScPP5"
+            target="_blank"
+          >
+            <div className="flex flex-row items-center gap-x-2">
+              <img
+                title="Open in Solscan"
+                alt="Open in Solscan"
+                src="/images/Solscan.png"
+                className="h-3 md:h-5 rounded-full"
+              />
+              <span
+                title="MEW1gQWJ3nEXg2qgERiKu7FAFj79PHvQVREQUzScPP5"
+                className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+              >
+                MEW1...cPP5
+              </span>
+            </div>
+            <span
+              title="total tokens"
+              className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+            >
+              3.7M MEW
+            </span>
+            <div>
+              <span
+                title="price"
+                className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+              >
+                {/* <span className="text-[#94E448]">&lt;$ </span>0.01 */}
+                <span className="text-[#94E448]">$ </span>
+                12,189.72
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      {/* 새 카드 */}
+      <div className="flex justify-center items-start flex-col">
+        <div
+          role="button"
+          className="relative"
+          style={{ opacity: "1", transform: "none" }}
+        >
+          <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+            <div className="w-24 md:w-36 h-24 md:h-36">
+              <img
+                className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+                src="/images/ZEUS.png"
+                alt="$ZEUS airdrop | zeusonsol.org"
+              />
+            </div>
+          </div>
+        </div>
+        {/* 여기부터가 텍스트영역 */}
+        <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+          <span
+            title="cat in a dogs world"
+            className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+          >
+            cat in a dogs world
+          </span>
+          <a
+            href="https://solscan.io/token/2SwUSgSMCUSGowWjzeF8A1kvS5BKNia447tqrTh9KmCU"
+            target="_blank"
+          >
+            <div className="flex flex-row items-center gap-x-2">
+              <img
+                title="Open in Solscan"
+                alt="Open in Solscan"
+                src="/images/Solscan.png"
+                className="h-3 md:h-5 rounded-full"
+              />
+              <span
+                title="2SwUSgSMCUSGowWjzeF8A1kvS5BKNia447tqrTh9KmCU"
+                className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+              >
+                2SwU...KmCU
+              </span>
+            </div>
+            <span
+              title="total tokens"
+              className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+            >
+              15,750 aZEUS
+            </span>
+            <div>
+              <span
+                title="price"
+                className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+              >
+                {/* <span className="text-[#94E448]">&lt;$ </span>0.01 */}
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center items-start flex-col">
+        <div
+          role="button"
+          className="relative"
+          style={{ opacity: "1", transform: "none" }}
+        >
+          <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+            <div className="w-24 md:w-36 h-24 md:h-36">
+              <img
+                className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+                src="/images/DODG.png"
+                alt="DODG the Elephant Shrew"
+              />
+            </div>
+          </div>
+        </div>
+        {/* 여기부터가 텍스트영역 */}
+        <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+          <span
+            title="DODG the Elephant Shrew"
+            className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+          >
+            DODG the Elephant Shrew
+          </span>
+          <a
+            href="https://solscan.io/token/FaYTnfmPK8uP4dvtECypG3ugCC3wQrG27pwkB1YkhXsG"
+            target="_blank"
+          >
+            <div className="flex flex-row items-center gap-x-2">
+              <img
+                title="Open in Solscan"
+                alt="Open in Solscan"
+                src="/images/Solscan.png"
+                className="h-3 md:h-5 rounded-full"
+              />
+              <span
+                title="FaYTnfmPK8uP4dvtECypG3ugCC3wQrG27pwkB1YkhXsG"
+                className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+              >
+                FaYT...hXsG
+              </span>
+            </div>
+            <span
+              title="total tokens"
+              className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+            >
+              456 DODG
+            </span>
+            <div>
+              <span
+                title="price"
+                className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+              >
+                {/* <span className="text-[#94E448]">&lt;$ </span>0.01 */}
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center items-start flex-col">
+        <div
+          role="button"
+          className="relative"
+          style={{ opacity: "1", transform: "none" }}
+        >
+          <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+            <div className="w-24 md:w-36 h-24 md:h-36">
+              <img
+                className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+                src="/images/PLANK.jpg"
+                alt="Planktos"
+              />
+            </div>
+          </div>
+        </div>
+        {/* 여기부터가 텍스트영역 */}
+        <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+          <span
+            title="Planktos"
+            className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+          >
+            Planktos
+          </span>
+          <a
+            href="https://solscan.io/token/DVuaDuQdPZ6H49inC2Xoyx7BpLAAJTPPChSfHuGpy8X4"
+            target="_blank"
+          >
+            <div className="flex flex-row items-center gap-x-2">
+              <img
+                title="Open in Solscan"
+                alt="Open in Solscan"
+                src="/images/Solscan.png"
+                className="h-3 md:h-5 rounded-full"
+              />
+              <span
+                title="DVuaDuQdPZ6H49inC2Xoyx7BpLAAJTPPChSfHuGpy8X4"
+                className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+              >
+                DVua...y8X4
+              </span>
+            </div>
+            <span
+              title="total tokens"
+              className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+            >
+              5 PLANK
+            </span>
+            <div>
+              <span
+                title="price"
+                className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+              >
+                <span className="text-[#94E448]">&lt;$ </span>0.01
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center items-start flex-col">
+        <div
+          role="button"
+          className="relative"
+          style={{ opacity: "1", transform: "none" }}
+        >
+          <div className="w-24 md:w-36 h-24 md:h-36 mx-5 flex justify-center">
+            <div className="w-24 md:w-36 h-24 md:h-36">
+              <img
+                className="rounded-full bg-[#1C1C1C] border-primary object-cover w-24 md:w-36 h-24 md:h-36"
+                src="/images/RIP_DOGE.png"
+                alt="RIP DOGE"
+              />
+            </div>
+          </div>
+        </div>
+        {/* 여기부터가 텍스트영역 */}
+        <div className="w-24 md:w-36 flex flex-col items-start ml-5 mt-2 vt323-regular">
+          <span
+            title="RIP DOGE"
+            className="w-24 md:w-36 md:text-lg leading-7 md:leading-8 capitalize truncate text-white"
+          >
+            RIP DOGE
+          </span>
+          <a
+            href="https://solscan.io/token/R1PvYksqSofUnZCCvQyzHqFSGfqSugPQiXksGW8Ck6y"
+            target="_blank"
+          >
+            <div className="flex flex-row items-center gap-x-2">
+              <img
+                title="Open in Solscan"
+                alt="Open in Solscan"
+                src="/images/Solscan.png"
+                className="h-3 md:h-5 rounded-full"
+              />
+              <span
+                title="R1PvYksqSofUnZCCvQyzHqFSGfqSugPQiXksGW8Ck6y"
+                className="text-sm md:text-lg truncate text-white w-16 md:w-24"
+              >
+                R1Pv...Ck6y
+              </span>
+            </div>
+            <span
+              title="total tokens"
+              className="text-xs sm:text-lg md:vt323-regular truncate w-24 md:w-36 text-white"
+            >
+              67.3M RIPDOGE
+            </span>
+            <div>
+              <span
+                title="price"
+                className="text-xs sm:text-lg truncate text-white w-24 md:w-36 h-7"
+              >
+                <span className="text-[#94E448]">&lt;$ </span>0.01
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BodyCard_NormalToken;
